46dc45356b88b734cdbea5c5b45f1f9449ac50be
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-10-08 15:40:44 +0200

    🐛 fix: dont select an airport if search is empty

--- src/components/AirportDropDownView.tsx
+++ src/components/AirportDropDownView.tsx
@@ -25,19 +25,22 @@ export const AirportDropDownView = ({ dropdownId, title, autofocus }: AirportDro
             autoFocus={autofocus}
             throttle
         >
-            {airports?.results.map((airport) => (
-                <Form.Dropdown.Item
-                    key={airport.column_1}
-                    value={JSON.stringify({
-                        code: airport.column_1,
-                        name: airport.airport_name,
-                        country: airport.country_code,
-                        lat: airport.latitude,
-                        lon: airport.longitude,
-                    })}
-                    title={`[${airport.column_1}] ${airport.airport_name} ${countryList[airport.country_code].flag}`}
-                />
-            ))}
+            {searchText.length > 0 &&
+                airports?.results.map((airport) => (
+                    <Form.Dropdown.Item
+                        key={airport.column_1}
+                        value={JSON.stringify({
+                            code: airport.column_1,
+                            name: airport.airport_name,
+                            country: airport.country_code,
+                            lat: airport.latitude,
+                            lon: airport.longitude,
+                        })}
+                        title={`[${airport.column_1}] ${airport.airport_name} ${
+                            countryList[airport.country_code].flag
+                        }`}
+                    />
+                ))}
         </Form.Dropdown>
     )
 }
